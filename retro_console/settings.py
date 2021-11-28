4b24c601c6dba6dd767f44b5a09b812278c35cfe
craigmantona <craigmantona@hotmail.com>
2021-11-28 21:06:17 +0000

    Heroku deployment part 2

--- retro_console/settings.py
+++ retro_console/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-(5+=lons_*n)x1464s^1r=mq*2gqogbb1kg2)n02gwz7&dkx37
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['retro-console-ms4.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,12 +122,17 @@ WSGI_APPLICATION = 'retro_console.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
