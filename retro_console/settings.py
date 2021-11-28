22bf52d843b4dc0dcbf25786c46bada805198226
craigmantona <craigmantona@hotmail.com>
2021-11-28 21:20:16 +0000

    Removedsecret key and set debug

--- retro_console/settings.py
+++ retro_console/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(5+=lons_*n)x1464s^1r=mq*2gqogbb1kg2)n02gwz7&dkx37'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['retro-console-ms4.herokuapp.com', 'localhost']
 
